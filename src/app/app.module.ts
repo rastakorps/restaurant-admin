289c77922419e254fe548fb0e4973b2dceefe0a2
unknown <jose.vega@nuvem.mx>
2022-03-01 00:54:59 -0600

    restaurant|Se remueve commonModule en app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule, 
+    CommonModule,
     IonicModule.forRoot(), 
     AppRoutingModule, 
     HttpClientModule
